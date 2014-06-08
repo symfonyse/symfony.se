c894f811376972cde0efdd3f7d92a4862f8d91b1
Nyholm <tobias.nyholm@gmail.com>
2014-06-08 17:58:59 +0200

    minor fix

--- app/AppCache.php
+++ app/AppCache.php
@@ -3,8 +3,6 @@
 require_once __DIR__.'/AppKernel.php';
 
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class AppCache extends HttpCache
 {
